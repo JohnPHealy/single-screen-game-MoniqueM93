ad87d782946764d925721d47d79075681719bb24
MoniqueM93 <monique.murphy.93@live.com>
2021-03-09 00:48:05 +0000

    coding tests

--- SingleScreen/Assets/Scripts/PlayerMovement.cs
+++ SingleScreen/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,86 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D myRB;
-    public float moveSpeed;
-    Vector2 movement;
-
+    public float maxSpeed;
+    public float acceleration;
+    public float currentSpeed;
+    public float verticalSpeed;
+    public float jumpforce;
+    public bool grounded;
+    public Transform animatorGO;
+    float animatorGOInitial;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+        animatorGOInitial = animatorGO.localScale.x;
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Moves Forward and back along z axis                           //Up/Down
-        transform.Translate(Vector2.up * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
-        //Moves Left and right along x Axis                               //Left/Right
-        transform.Translate(Vector2.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        currentSpeed = myRB.velocity.x;
+
+        verticalSpeed = myRB.velocity.y;
+
+        float move = Input.GetAxis("Horizontal"); //checking for left and right input on keyboard or controller
+        print("move is " + move);
+
+        //Flip Animator GameObject based on input
+        if (move > 0)
+        {
+            animatorGO.localScale = new Vector3(animatorGOInitial, animatorGO.localScale.y, animatorGO.localScale.z);
+        }
+        else if (move < 0)
+        {
+            animatorGO.localScale = new Vector3(-animatorGOInitial, animatorGO.localScale.y, animatorGO.localScale.z);
+        }
+
+        //end flipping animator
+
+        if (Mathf.Abs(currentSpeed) < maxSpeed)
+        {
+            myRB.AddRelativeForce(new Vector2(move * acceleration, 0)); //Adding Relative force to the rigidbody2d depending on the input vector
+        }
+
+        //left and right move code ends
+
+        //animator speed code
+        anim.SetFloat("speed", Mathf.Abs(currentSpeed + move));
+        anim.SetFloat("vspeed", verticalSpeed);
+        //end animator speed code
+
+        //jump code begins
+
+        if (Input.GetKeyDown("space") && grounded == true)
+        {
+            myRB.AddForce(new Vector2(0, jumpforce));
+        }
+
+        //jump code ends
     }
+    //grounded check code starts
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            grounded = true;
+            anim.SetBool("grounded", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            grounded = false;
+            anim.SetBool("grounded", false);
+        }
+    }
+
+    //grounded check code ends
 }
